2ee9830f0fc8ec3181012812fccff6bd4ba2fd35
Nishant-Kumar-Sah <nishant.sah2001@gmail.com>
2025-03-29 23:13:59 +0530

    setup communication between submission-service and problem-service

--- src/repository/submissionRepository.js
+++ src/repository/submissionRepository.js
@@ -5,8 +5,13 @@ class submissonRepository {
         this.submissionModel = Submission
     }
     async createSubmission(submission) {
-        const response = await this.submissionModel.create(submission)
-        return response
+        try {
+            const response = await this.submissionModel.create(submission)
+            console.log(`Succesfully created a submission in DB`)
+            return response
+        }catch(error) {
+            console.error(`Failed to create a submission in DB: ${error}`)
+        }
     }
 }
 
--- src/services/submissionServices.js
+++ src/services/submissionServices.js
@@ -1,3 +1,4 @@
+const { fetchProblemDetails } = require('../apis/problemAdminApi');
 const submissionProducer = require('../producers/submissionQueueProducer')
 
 class SubmissionService{
@@ -10,8 +11,24 @@ class SubmissionService{
     }
 
     async addSubmission(submissionPayload) {
-        // console.log(`Submission Payload Recieved ${submissionPayload}`)
+        console.log(`Submission Payload Recieved `)
         console.log(submissionPayload)
+        const problemId = submissionPayload.problemId;
+        console.log(`Problem Id : ${problemId}`)
+        const problemAdminApiResponse = await fetchProblemDetails(problemId)
+
+        // if(!problemAdminApiResponse){
+        //     throw new SubmissionCreationError(`Failed to create a submission in the repository layer`)
+        // }
+        console.log(`Printing problem admin api response`)
+        const codeStubs = problemAdminApiResponse.data.codeStubs
+        console.log(codeStubs)
+
+        const languageCodeStub = codeStubs.find(codeStub => codeStub.language.toLowerCase === submissionPayload.language.toLowerCase)
+        console.log(`CodeStub for language ${submissionPayload.language}`)
+        console.log(languageCodeStub)
+
+        submissionPayload.code = languageCodeStub.startSnippet +"\n\n"+ submissionPayload.code + "\n\n"+ languageCodeStub.endSnippet
         const submission = await this.submissionRespository.createSubmission(submissionPayload)
         if(!submission){
             throw {message : "Not able to create submission"}
